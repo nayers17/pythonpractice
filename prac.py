6020f4065e5f39cac646979d7f57548216ec211b
Nathan Ayers <nathanayers17@gmail.com>
2024-11-24 03:43:25 -0500

    yup

--- prac.py
+++ prac.py
@@ -87,5 +87,81 @@ class Dog:
 dog = Dog("Harlow the Pooch", "cocker spanuil")
 print(dog)
 
-    
+# iterating through a list 
+fruits = ["apple", "banana", "orange"]
+for fruit in fruits:
+    print(f"I love {fruit}")
+
+# countdown example
+count = 5
+while count > 0:
+    print(f"counting down: {count}")
+    count -= 1
+print(f"blast off!")
+
+count = 8
+while count > 3:
+    print(f"monkey takes one banana and the last number is the amount of monkeys that went without a banana: {count}")
+    count -= 1
+print(f"3 monkeys did not get banana :(")
+
+# conditionasl: let you execute code only if a specifc requirement is met
+
+# if else example
+
+temperature = 90
+
+if temperature > 85: 
+    print("it's hot out here!")
+
+elif temperature < 65:
+    print ("brrrrr so cold!")
+
+else:
+    print(f"omg feel so good")
+
+
+# write some data to a file
+
+with open("example.txt", "w") as file:
+    file.write("hello, python practice!!!\n")
+    file.write("This is line 2.\n")
+
+with open("exampleton.txt", "w") as file:
+    file.write("wasssup\n")
+    file.write("crackalacka")
+
+with open("yehayehacha.txt", "w") as file:
+    file.write("yeha\n")
+    file.write("yeha\n")
+    file.write("yehacha\n")
+
+with open("yehayehacha.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+with open("yehayehacha.txt", "a") as file:
+    file.write("GREEN EGGS N HAAAAAAAAAAAAAAAM!!!!!!!!\n")
+    file.write("PEANUT BUTTER N JAAAAAAAAAAAAAM!!!!!!!!!!!\n")
+
+with open("yehayehacha.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+# write numbers to a file
+
+
+with open("numbers.txt", "w") as file:
+    for i in range (1, 11):
+        file.write(f"{i}\n")
+
+# read and heck in tfunmerbs even or odd
+
+with open("numbers.txt", "r") as file:
+    for line in file:
+        number = int(line.strip())
+        if number % 2 == 0:
+            print(f"{number} is even")
+        else: 
+            print(f"{number} is odd")
 
